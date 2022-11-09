03bfc405d614a3ed5365bdcb329e541e8b50549b
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-11-09 09:47:15 +0500

    Updated main.js
    Just added some more code

--- Random Choice Picker/main.js
+++ Random Choice Picker/main.js
@@ -1,12 +1,65 @@
-const tagsElement = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
+const tagsDiv = document.getElementById("tags");
 
+textarea.addEventListener("keyup" , (e) => {
+  createTags(e.target.value);
 
+  if(e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = ""
+    }, 10)
+  }
 
-textarea.addEventListener('keyup', (e) => {
-  createTags(e.target.value)  //Get the element that triggered a specific event
+  randomSelect();
 });
 
 function createTags(input){
-    console.log(input);
+  const arrayOfTags = input.split(",").filter(tag => tag.trim() !== "").map(tag => tag.trim());
+  
+  tagsDiv.innerHTML = "";
+
+  arrayOfTags.forEach(tag => {
+    const tagsElement = document.createElement("span");
+    tagsElement.classList.add("tag");
+    tagsElement.innerText = tag;
+    tagsDiv.appendChild(tagsElement);
+  });  
+}
+
+function randomSelect(){
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag)
+
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100);
+
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag)
+    }, 100);
+
+  }, 30 * 100);
+}
+
+function pickRandomTag(){
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag){
+  tag.classList.remove("highlight")
 }
