e45a0820bf87cf4469e4163f153fe06655169d84
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-12-07 22:19:13 +0500

    Added-Login functionalities
    Added some code (including UI, open & close bar functionalities, email & password verification) in SMIT Attendance Application

--- SMIT Attendance Application/script.js
+++ SMIT Attendance Application/script.js
@@ -1,10 +1,56 @@
 const adminButton = document.getElementById("admin-button");
 const studentButton = document.getElementById("student-button");
-const closePanelButton = document.getElementById("close-panel");
+const studentCloseButton = document.getElementById("student-close-panel-button");
+const adminCloseButton = document.getElementById("admin-close-panel-button");
+const submitButton = document.getElementById("submit-button");
 
-studentButton.addEventListener("click", popupPanel)
-
-function popupPanel(){
-    document.getElementsById("admin-sign-in-panel").style.display = "none !important";
-    console.log("Button Working")
+if (studentCloseButton || adminCloseButton) {
+    studentCloseButton.addEventListener("click", popupPanel(3)); 
+    adminCloseButton.addEventListener("click", popupPanel(4));
 }
+
+
+
+const adminEmail = document.getElementById("admin-email");
+const adminPassword = document.getElementById("admin-password");
+
+const adminList = [{email: "moin@gmail.com", password: 1234}, {email: "shahzaib@gmail.com", password: 5678}]
+
+console.log(adminList[0].email)
+
+function openAdminPanel(event){
+    event.preventDefault();
+    console.log("Clicked")
+    for(let i = 0; i < adminList.length; i++){
+        if (adminEmail.value == adminList[i].email && adminPassword.value == adminList[i].password){
+            window.location.replace("admin-page.html");
+        } else if (adminEmail.value !== adminList[i].email || adminPassword.value !== adminList[i].password) {
+            document.getElementById("email-description").innerHTML = "Email (email or password is incorrect)"
+        }
+    }
+   
+    // if (document.getElementById("admin-email").value == "moin@gmail.com" && document.getElementById("admin-password").value == "1234"){
+    //     window.location.replace("admin-page.html");
+    // } else {
+    //     document.getElementById("email-description").innerHTML = "Email (email or password is incorrect)"
+    // }
+}
+
+function popupPanel(a){
+    if (a == 1) {
+        document.querySelector("form.admin-sign-in-panel").style = "display: block !important";
+    }
+    else if (a == 2){
+        document.querySelector("form.student-attendance-panel").style = "display: block !important";
+    } 
+    else if (a == 3){
+        document.querySelector("form.student-attendance-panel").style = "display: none !important";
+    }
+    else if (a == 4){
+        document.querySelector("form.admin-sign-in-panel").style = "display: none !important";
+    }
+        
+}
+
+
+
